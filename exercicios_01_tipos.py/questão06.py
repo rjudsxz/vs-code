32ce674b319dd9a56cd377070f809e15e9fd2501
rjudsxz <f.judson@academico.ifrn.edu.br>
2022-05-30 20:48:58 -0300

    questão06.py

---
+++ exercicios_01_tipos.py/questão06.py
@@ -0,0 +1,7 @@
+# Crie um programa onde o usuário possa digitar 10 valores numéricos e cadastre-os em uma lista.
+#  Caso o número já esteja lá dentro, ele não será adicionado. No final, 
+# serão exibidos todos os valores únicos digitados, em ordem crescente
+
+lista = []
+
+
